6a97df5a5c4dff9c6c29c3fd6222b55a3fd598d8
Mnihau <michal.barnas@onet.pl>
2023-06-28 18:35:03 +0200

    Update michal_zadanie.py

---
+++ zadanie1/michal_zadanie.py
@@ -0,0 +1,16 @@
+number = int(input("choose any number: "))
+fibo =()
+a = 0
+b = 1
+c = a + b
+if number == 1 or number == 2:
+    fibo = 1
+elif number == 0:
+    fibo = 0
+else:
+    for  number in range (2,number):
+        a = b
+        b = c
+        c = a + b
+        fibo = c
+print(fibo)
